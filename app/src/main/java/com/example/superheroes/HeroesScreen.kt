65dd69a68e86d10f251694d41441df20b82fb945
vitorsoaresferreira <vitorsoftwaredeveloper@gmail.com>
2025-08-12 20:46:16 -0300

    feat: implemets List Items Hero and dark mode

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -7,15 +7,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,6 +31,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroes.model.Hero
+import com.example.superheroes.model.HeroesRepository.heroes
 
 
 @Composable
@@ -45,7 +49,7 @@ fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
                 .padding(16.dp)
                 .size(72.dp)
         ) {
-            Column (modifier = Modifier.weight(1f)){
+            Column(modifier = Modifier.weight(1f)) {
                 Text(
                     text = stringResource(hero.name),
                     style = MaterialTheme.typography.displaySmall
@@ -66,17 +70,54 @@ fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
                     contentScale = ContentScale.Crop,
                     painter = painterResource(hero.imageResourceId),
                     contentDescription = null,
-                    )
+                )
             }
 
         }
     }
 }
 
+@Composable
+fun HeroList(
+    heroes: List<Hero>,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = { HeroesAppBar() },
+        modifier = modifier
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            items(heroes) {
+                HeroItem(
+                    hero = it,
+                    modifier = Modifier.padding(16.dp, 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeroesAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun CardHeroePreview() {
     SuperheroesTheme {
-        HeroItem(Hero(R.drawable.android_superhero1, R.string.hero1, R.string.description1))
+        HeroList(heroes)
     }
 }
